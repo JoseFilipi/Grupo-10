0d47de6cac9c8797c9a2048fd53d4e422100ae6c
luciano-freitas-melo <lucianodefreitasmelo@hotmail.com>
2021-10-18 18:02:07 -0300

    Arquivos que estavam na branch "HomeHTML-Gabriel"
    Como os arquivos que estavam na branch do Gabriel eram todos diferentes do que estavam na branch default, o github não permitiu fazer o pull request.
    
    Assim, foi necessário copiar os arquivos da outra branch e fazer um commit diretamente na default.

---
+++ AlterarDadosPF.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>Página de alteração de dados</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+
+        <table border="0" width="100%">
+            <tr>
+                <td align="center"><img src="imagens/Logo.jpeg" width="230"></td>
+            </tr>
+        </table>
+        <br><br>
+
+        <hr>
+        <p>Caso suas informações estejam desatualizadas ou necessitem de trocas devido algum erro de digitação, você pode troca-las preenchendo os dados abaixo novamente; só não se esqueça de colocar os dados corretos pois seu CPF está vinculado a alguns dados seus, caso esses dados não correspondam, problemas futuros poderão ser gerados!</p>
+        <hr><br><br>
+
+        dados relativos ao CPF:
+        <br><br> 
+
+        <table border=1 width="100%">
+            <tr>
+                <td>
+                    <form>
+                        Nome completo:<br>
+                        <input type="text" name="nome">
+                    </form>
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    <form>
+                        Data de nascimento:<br>
+                        <input type="text" name="nascimento" placeholder="00/00/0000">
+                    </form>
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    RG:<br>
+                    <input type="text" name="rg">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    CEP:<br>
+                    <input type="text" name="cep" placeholder="00000-000">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Número de celular:<br>
+                    <input type="text" name="cel" placeholder="(00) 00000-0000">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    E-mail:<br>
+                    <input type="text" name="email"><br><br>
+                    <strong>Não se esqueça de ter certeza que você tem acesso a este e-mail, pois caso você perca sua senha, será a partir dele que o seu acesso será reconcedido!</strong>
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Confirme o e-mail:<br>
+                    <input type="text" name="emailconf">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    <strong>Para alterar sua senha, será necessário entrar na área de login e ir em "esqueceu a senha"; assim será enviado um e-mail para você, esse processo é devido uma maior segurança.</strong>
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Possui ulgum plano de saúde?<br>
+                    <input type="radio" name="plano" value="sim">Sim
+                    <input type="radio" name="plano" value="não">Não
+                    <br><br>
+                    <input type="text" name="textoplano" placeholder="Se sim, qual?">
+                    <br><br>
+                    <input type="text" name="carteirinha" placeholder="Número da carteirinha">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Adicione uma nova foto ao perfil, ou então mande o mesmo arquivo de antes (opcional)<br><br>
+                    <img src="imagens/avatar.png" width="100">
+                    <br><br>
+                    <input type="file" name="foto">
+                </td>
+            </tr>
+        </table>
+        <br><br>
+
+        <button type="submit">
+            <a href="">Concluir atualização de dados</a> <!--Colocar o link da página: conclusão da atualizaçao de dados PF-->
+        </button>
+        
+    </body>
+
+</html>
---
+++ AlterarDadosPJ.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>Página de alteração de dados</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+
+        <table border="0" width="100%">
+            <tr>
+                <td align="center"><img src="imagens/Logo.jpeg" width="230"></td>
+            </tr>
+        </table>
+        <br><br>
+
+        <hr>
+        <p>Caso suas informações estejam desatualizadas ou necessitem de trocas devido algum erro de digitação, você pode troca-las preenchendo os dados abaixo novamente; só não se esqueça de colocar os dados corretos pois seu CNPJ está vinculado a alguns dados seus, caso esses dados não correspondam, problemas futuros poderão ser gerados!</p>
+        <hr><br><br>
+
+        Dados relativos ao CNPJ:
+        <br><br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Nome da clínica:<br>
+                    <input type="text" name="nome">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    CEP:<br>
+                    <input type="text" name="cep" placeholder="xxxxx-xxx">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Endereço:<br>
+                    <input type="text" name="endereço">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Número para contato:<br>
+                    <input type="text" name="cel" placeholder="(00) 00000-0000">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Horário de funcionamento:<br>
+                    <input type="text" name="horario">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    E-mail:<br>
+                    <input type="text" name="email">
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    confirme o e-mail:<br>
+                    <input type="text" name="emailconf"><br><br>
+                    <strong>Não se esqueça de ter certeza que você tem acesso a este e-mail, pois caso você perca sua senha, será a partir dele que o seu acesso será reconcedido!</strong>
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    Adicione uma foto para representar sua clínica no site (opcional)<br><br>
+                    <img src="imagens/avatarc.png" width="100">
+                    <br><br>
+                    <input type="file" name="foto">  
+                </td>
+            </tr>
+        </table>
+        <br>
+
+        <table border="1" width="100%">
+            Escreva uma breve descrição da sua clínica:<br>
+            <textarea></textarea>
+        </table>
+        <br><br>
+
+        <button>
+            <a href="">Concluir atualização de dados</a> <!--Colocar o link da página: conclusão de atualização de dados PJ-->
+        </button>
+
+    </body>
+
+</html>
