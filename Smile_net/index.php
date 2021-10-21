e91ca289a24193c9e51c205d09fe732a5360ae6a
luciano-freitas-melo <lucianodefreitasmelo@hotmail.com>
2021-10-21 14:47:12 -0300

    Versão final - Página Home

--- Smile_net/index.php
+++ Smile_net/index.php
@@ -39,71 +39,52 @@
 		</header>
 
 		<main>
-			<section class="carrossel">
-				<div id="slider">
-					<img class="selected" src="Imagens/Logo.jpeg" alt="Image1">
-					<img src="https://draluisapolcaro.com.br/wp-content/uploads/2020/07/Tipos-de-dentes-e-suas-func%CC%A7o%CC%83es-1.jpg" alt="Image2">
-					<img src="https://cdn.xxl.thumbs.canstockphoto.com.br/azul-tools-imagem-rendering-dente-dentista-fundo-3d-banco-de-fotos_csp71894636.jpg" alt="Image3">
-					<img src="https://thumbs.dreamstime.com/z/os-dentes-brancos-saud%C3%A1veis-est%C3%A3o-sorrindo-e-o-dente-com-c%C3%A1rie-%C3%A9-triste-em-ferramentas-verdes-espelho-do-fundo-dentista-da-h-140280036.jpg" alt="Image4">
-				</div>
-				<script src="script.js"></script>
-			</section>
 			<section class="principal">
-				<img src="Imagens/fundo-home.jpg" class="fundo-home">
+				<img src="Imagens/fundo-home.jpg" class="fundo-home" alt="Fundo - dentista em sala médica">
 				<div class="conteudo-principal">
 					<h1 class="titulo-principal">Fichas de Anamnese a um <em>clique!</em></h1>
 					<p>A <strong>Smile Anamnese</strong> traz para as pessoas e clínicas um suporte de pré-atendimento no quesito de melhoria no atendimento e ganho de tempo no atendimento ao cliente.</p>
 					<p>Sendo um programa <em>gratuito</em> que visa facilitar o processo, tanto de marcação de uma consulta em uma clínica odontológica, quanto o questionário de anamnese que é necessário preencher antes de qualquer consulta.</p>			
 					<p>Através do nosso site você poderá ter acesso a diversas clínicas que estão perto de você e também decidir qual a melhor, com base em: informações da clínica, tipos de serviços ofertados, avaliaçãoes de outros pacientes e muito mais!</p>
 					<h2>Veja agora mesmo as vantagens de cada conta e obtenha as facilidades ofertadas pelo programa!</h2>
-					<button class="btn-confira">Confira</button>
+					<a href="logar.php"><button class="btn-confira">Confira</button></a>
 				</div>
 				<div class="img-ficha">
 					<img src="Imagens/ficha-anamnese.png" alt="Mão usando ficha de anamnese">
 				</div>
 			</section>
 
-			<section>
-				<p id="missao"><em>Nossa missão é: <strong>"Proporcionar ao cliente um atendimento mais ágil e as clínicas melhor suporte de pré-atendimento"</strong>.</em></p>
-					
-				<p>Oferecemos no nosso site agilidade em pré-inscrição, para futuros atendimentos em clinicas vinculadas.</p>
-			</section>
-			
-			<section class="mapa">
-				<h3 class="titulo-principal">Nossa central física - Smile Anamnese - </h3>
-				<p>Nosso estabelecimento está localizado no coração da cidade.</p>
-
-				<div class="mapa-conteudo">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3835.4588891090566!2d-48.0465915!3d-15.98961!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a2a8c49ce3677%3A0x447f05b6f05fa281!2sUniversidade%20de%20Bras%C3%ADlia%20-%20Gama!5e0!3m2!1spt-BR!2sbr!4v1632444896226!5m2!1spt-BR!2sbr" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-
-
-				</div>
-			</section>
-
 			<section class="beneficios">
-				<h3 class="titulo-principal">Benefícios</h3>
-
-				<div class="conteudo-beneficios">
-					<ul class="lista-beneficios">
-						<li class="itens">Cadastro e login no site;</li>
-						<br>
-						<li class="itens">Para cliente e Clínicas;</li>
-						<br>
-						<li class="itens">Ótimo serviço de pesquisar clínicas;</li>
-						<br>
-						<li class="itens">Preenchimento de Ficha de Anamnese;</li>
-						<br>
-						<li class="itens">Agilidade nas consultas;</li>
-						<br>
-						<li class="itens">Possibilidade de Pesquisar clínicas credenciadas;</li>
-						<br>
-						<li class="itens">O cliente em primeiro lugar;</li>
-					</ul><img src="Imagens/beneficios.jpg" class="imagem-beneficios">
+				<h1 class="titulo-secundario">Benefícios</h1>
+				<div class="caixa">
+					<div class="conteudo-beneficios">
+						<ul class="lista-beneficios">
+							<li class="itens">Cadastro e login no site;</li>
+							<br>
+							<li class="itens">Para cliente e Clínicas;</li>
+							<br>
+							<li class="itens">Ótimo serviço de pesquisar clínicas;</li>
+							<br>
+							<li class="itens">Preenchimento de Ficha de Anamnese;</li>
+							<br>
+							<li class="itens">Agilidade nas consultas;</li>
+							<br>
+							<li class="itens">Possibilidade de Pesquisar clínicas credenciadas;</li>
+							<br>
+							<li class="itens">O cliente em primeiro lugar;</li>
+						</ul>
+					</div>
+					<div class="carrossel">
+						<div id="slider">
+							<img class="selected" src="Imagens/beneficios.jpg" alt="Dente em projeção futurista">
+							<img src="Imagens/dentes-carrossel.jpg" alt="desenho de dentes e utensílios odontológicos">
+							<img src="Imagens/dente neon-carrossel.jpg" alt="Dente e utensílios odontológicos em neon">
+							<img src="Imagens/exame dente-carrossel.jpg" alt="Exame odontológico em paciente">
+						</div>
+						<script src="js/carrossel.js"></script>
+					</div>
 				</div>
-
-				<div class="video">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/0-5pVlvVvpc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-				</div>
+				
 			</section>
 		</main>
 
@@ -116,7 +97,5 @@
 		<script type="text/javascript" src="js/bootstrap.min.js"></script>
     	<noscript>Javascript de carregamento do Framework Bootstrap</noscript>
     	<script type="text/javascript" src="js/jquery.easing.min.js"></script>
-
-
 	</body>
 </html>
